f8b1ca3077002961c180c123745020f38e853d35
michaeltwofish <USER@DOMAIN>
2008-09-19 02:55:47 +0000

    Add a link to older posts on the community blog. This will only work where there is a rewrite rule in the db like this  '%^(?:page/(?P<page>[1-9]|[1-9][0-9]+))/?$%' as there is on hp.o. Otherwise it will just show home again.
    git-svn-id: http://svn.habariproject.org/habari-extras/themes/rino/trunk@961 f4a90942-1786-4aa1-b092-57c31935ddc3

--- home.php
+++ home.php
@@ -9,29 +9,29 @@ $theme->display('header');
 ?>
 
 	<div class="column span-17 colborder">
-	  <div class="column span-10" id="masthead">
-	  	<h1>habari</h1>
-	  	<h2>spread the news</h2>
-	  </div>
+		<div class="column span-10" id="masthead">
+			<h1>habari</h1>
+			<h2>spread the news</h2>
+		</div>
 
-	  <div class="column span-7 last" id="subhead">
-	  	<p><b>ha&middot;bar&middot;i - </b> noun<br>
-	  	Definition &mdash; Swahili greeting: What's the news?
+		<div class="column span-7 last" id="subhead">
+			<p><b>ha&middot;bar&middot;i - </b> noun<br>
+			Definition &mdash; Swahili greeting: What's the news?
 		</p>
-	  </div>
+		</div>
 
-	  <div class="column span-17 last" id="elevator">
-	  	<p>Habari is next-generation blogging. <a href="download">Download</a> Habari and help our <a href="http://wiki.habariproject.org/en/Getting_Involved">community</a> design the future of blogs.</p>
-	  </div>
+		<div class="column span-17 last" id="elevator">
+			<p>Habari is next-generation blogging. <a href="download">Download</a> Habari and help our <a href="http://wiki.habariproject.org/en/Getting_Involved">community</a> design the future of blogs.</p>
+		</div>
 
-	  <hr>
+		<hr>
 
-	  <div id="media">
+		<div id="media">
 		<div id="noUpdate">
 			<p>The video content presented here requires JavaScript to be enabled and the  latest version of the Macromedia Flash Player. If you are using a browser with JavaScript disabled please enable it now. Otherwise, please update your version of the free Flash Player by <a href="http://www.macromedia.com/go/getflashplayer">downloading here</a>. </p>
 		</div>
-	  </div>
-	  <script type="text/javascript">
+		</div>
+		<script type="text/javascript">
 		// <!--
 		var so = new SWFObject('<?php Site::out_url('theme')?>/player.swf','mpl','595','295','9');
 		so.addParam('allowscriptaccess','always');
@@ -39,26 +39,26 @@ $theme->display('header');
 		so.addParam('flashvars','&file=http://blip.tv/file/get/Ringmaster-HabariInstall846.flv');
 		so.write('media');
 		// -->
-	  </script>
+		</script>
 
-	  <hr>
+		<hr>
 
-	  <div class="column span-17" id="screencast">
+		<div class="column span-17" id="screencast">
 		<p>We've created a few <a href="screencasts">screencasts</a> for you to get a better appreciation of the software in action. We also have <a href="screenshots">screenshots</a> if you are looking for those, instead.</p>
-	  </div>
+		</div>
 
-	  <div class="column span-17" id="sellingpoints">
+		<div class="column span-17" id="sellingpoints">
 		<?php
 		// grab 'home' post
 		$homepost = Posts::get( array( 'slug' => 'home' ) );
 		echo $homepost[0]->content_out;
 		?>
-	  </div>
+		</div>
 	</div>
 
 	<div class="column span-6 last" id="sidebar">
 
-	  <div id="community-blog">
+		<div id="community-blog">
 		<h2>Community Blog</h2>
 
 		<?php $post = $posts[0]; ?>
@@ -68,9 +68,10 @@ $theme->display('header');
 			<?php echo $post->content_excerpt; ?>
 
 		</div>
-	  </div>
+			<a href="<?php echo Site::get_url( 'habari' ) . '/page/1'; ?>">Previous Posts</a>
+		</div>
 
-	  <div id="user-mailing-list">
+		<div id="user-mailing-list">
 		<h2><a href="http://groups.google.com/group/habari-users" class="discuss-link">Users Mailing List</a></h2>
 
 		<?php if ( isset($feedlist) && is_array($feedlist) ) {
@@ -79,9 +80,9 @@ $theme->display('header');
 			<p>Feed not available</p>
 		<?php } ?>
 
-	  </div>
+		</div>
 
-	  <div id="featured-extras">
+		<div id="featured-extras">
 		<h2>Plugins and Themes</h2>
 
 		<p>Some popular plugins include:</p>
@@ -93,7 +94,7 @@ $theme->display('header');
 			<li>Defensio spam filtering</li>
 			<li>lifestreaming</li>
 		</ul>
-	  </div>
+		</div>
 	</div>
 
 
