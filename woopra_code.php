21812168b3993e4b267d32f4d08068d0cd167199
chrismeller <chris@doesnthaveone.com>
2008-10-27 18:08:35 +0000

    Adding the Woopra code that one would think should be a plugin...
    git-svn-id: http://svn.habariproject.org/habari-extras/themes/rino/trunk@1207 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ woopra_code.php
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+var woopra_id = '1253971658';
+</script>
+<script src="http://static.woopra.com/js/woopra.js"></script>
+<script type="text/javascript" src="http://cetrk.com/pages/scripts/0009/7191.js"> </script>
+
