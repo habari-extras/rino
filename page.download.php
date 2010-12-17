2376e522bb137b28f82300d0894544594e6042d4
chrismeller <chris@doesnthaveone.com>
2010-12-17 18:31:27 +0000

    Update the download page.
    git-svn-id: http://svn.habariproject.org/habari-extras/themes/rino/trunk@3570 f4a90942-1786-4aa1-b092-57c31935ddc3

--- page.download.php
+++ page.download.php
@@ -26,12 +26,13 @@ $theme->display('header');
 		<div id="downloading">
 <?php
 /*
-Options::set('download_url', 'http://habariproject.org/dist/habari-0.5.2.zip');
-Options::set('download_version', '0.5.2');
+// change the lines below to update the version
+Options::set('download_url', 'http://habariproject.org/dist/habari-0.6.6.zip');
+Options::set('download_version', '0.6.6');
 */
 ?>
 		<h3><a href="<?php Options::out('download_url'); ?>">Download The Current Stable Version</a></h3>
-		<p>The current stable version of Habari is <b><?php echo Options::out('download_version'); ?></b></p>
+		<p>The current stable version of Habari is <b><?php echo Options::out('download_version'); ?></b>.  <a href="<?php Options::out('download_url'); ?>" style="border:1px solid #4D9EBC; padding: 3px;">Download it now!</a></p>
 		</div>
 		<div id="releasenotes">
 			<h4>Release Notes</h4>
@@ -39,10 +40,12 @@ Options::set('download_version', '0.5.2');
 		</div>
 		<div id="alternate-downloads">
 			<h4>Alternate Downloads</h4>
-			<p>A snapshot of the current development version is available for <a href="http://habariproject.org/dist/habari_head.zip">down
-load</a>.</p>
-			<p><b>Beware:</b> Using the development snapshot may cause breakage on your site.  If dealing with in-development glitches is 
-not your cup of tea, try the latest stable download, above.</p>
+			<p>A snapshot of the current development version is available for <a href="http://habariproject.org/dist/habari_head.zip">download</a>.</p>
+			<p><b>Beware:</b> Using the development snapshot may cause breakage on your site.  If dealing with in-development glitches is not your cup of tea, try the latest stable download, above.</p>
+
+			<h4>Compatibility Test</h4>
+			<p>You can test whether your server is compatible with the development snapshot of Habari by uploading the <a href="https://trac.habariproject.org/habari/browser/trunk/tests/requirements.php?format=txt">requirements.php</a> file to your host and viewing it through your browser.  This file will show you what needs to be changed on your server to enable Habari, if required.</p>
+
 			<h4>Subversion Access</h4>
 			<p>The Habari subversion repository is located here:<br />
 			<a href="http://svn.habariproject.org/habari/">http://svn.habariproject.org/habari/</a>
@@ -50,8 +53,7 @@ not your cup of tea, try the latest stable download, above.</p>
 			<p>To check out only the files required for your web root, execute this command within your web root directory:<br />
 			<code>svn co http://svn.habariproject.org/habari/trunk/htdocs/ .</code>
 			</p>
-			<p>Learn more <a href="http://wiki.habariproject.org/en/Subversion_and_applying_patches">about subversion access</a> to Habari
- source.</p>
+			<p>Learn more <a href="http://wiki.habariproject.org/en/Subversion_and_applying_patches">about subversion access</a> to Habari source.</p>
 		</div>
 	</div>
 
